990a0a727e248a868bfd8ccef98f4b8bfe4f4d7a
Greg <gregory.cottone@yahoo.it>
2022-12-05 23:20:52 +0100

    Install packages

--- Image_Segmentation/Image_segmentation_main.py
+++ Image_Segmentation/Image_segmentation_main.py
@@ -6,4 +6,21 @@ import numpy as np
 import skimage.io
 import matplotlib
 import matplotlib.pyplot as plt
-from google.colab import drive
+import warnings
+
+# pip install keras==2.2.5
+
+# Fetching the root directory
+# ROOT_DIR = os.path.abspath("../")
+
+warnings.filterwarnings("ignore")
+# Importing Mask RCNN 
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Heading to the coco directory
+sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  
+#importing coco.py
+import coco
+# matplotlib inline
